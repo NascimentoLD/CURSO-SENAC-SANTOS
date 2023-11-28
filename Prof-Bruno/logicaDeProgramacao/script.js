5cfeaefa8a685d0020e437ae3e4694117d6b7e30
João Pedro <joao.pngoncalves@senacsp.edu.br>
2023-11-28 17:25:05 -0300

    Aprendemos sobre a estrutura IF e ELSE

--- Prof-Bruno/logicaDeProgramacao/script.js
+++ Prof-Bruno/logicaDeProgramacao/script.js
@@ -4,6 +4,7 @@
 
 // let num2 = prompt("Digite outro número:")
 
+// _______________________________________________________
 
 // let soma; 
 
@@ -22,7 +23,7 @@
 // alert( 'O resultado da operação é: ' +soma);
 
 
-
+// _______________________________________________________
 
 
 // let nt1 = prompt('Digite a sua nota na primeira prova: ')
@@ -39,6 +40,8 @@
 
 // alert('A media do aluno é: '+media);
 
+
+// _______________________________________________________
 
 
 
@@ -61,6 +64,9 @@
 // let vltotal;
 
 
+// _______________________________________________________
+
+
 // if(dist <= 200){
 //     vltotal = Number(dist)*0.50;
 // } else{
@@ -71,6 +77,8 @@
 //     "O valor a ser pago é :" +vltotal
 // )
 
+
+// _______________________________________________________
 
 
 // let nt1 = prompt("Digite a nota do primeiro semestre: ");
@@ -91,15 +99,97 @@
 // }
 
 
+// _______________________________________________________
 
-let name = prompt("Qual o nome do funcionario:");
-let salario = prompt("Qual o salario desse funcionario: ");
-let ano = prompt("Quantos anos esse funcionario trabalha na empresa: ");
 
-let reajuste;
+// let name = prompt("Qual o nome do funcionario:");
+// let sal = prompt("Qual o salario desse funcionario: ");
+// let temp = prompt("Quantos anos esse funcionario trabalha na empresa: ");
+// let conv, self;
 
-if(reajuste <= 3){
-    Number(salario)*0.03;
-} else if(reajuste >= 5 ){
+// if(temp <= 3){
+//     conv = Number(sal) * 0.03;
+// }
 
+// else if(temp > 3 && temp <= 10){
+//     conv = Number(sal) * 0.125;
+// }
+
+// else{
+//     conv = Number(sal) * 0.2;
+// }
+
+//     salf = Number(conv) + Number(sal);
+
+// alert(name + "Seu salario teve um aumento de: " + "R$ "  + conv + " e seu novo salario é de: " + "R$ " + salf );
+
+
+// _______________________________________________________
+
+
+// let larg = prompt("Digite a largura do terreno: ");
+// let comp = prompt("Digite o comprimento do terreno: ");
+// let m;
+
+// m = Number(larg) * Number(comp);
+
+// if(m < 100){
+//     alert("Terreno Popular");
+// }else if(m >= 100 && m <= 500){
+//     alert(" Terreno Mater");
+// }else{
+//     alert("Terreno Vip");
+// }
+
+
+// _______________________________________________________
+
+// let hora = prompt("Digite quantas horas você praticou de atividades fisicas: ")
+// let ganhou = 0.05;
+// let pontos, resultado;55
+
+// if(hora <= 10){
+//     pontos = Number(hora) * 2;
+// }else if(hora > 10 && hora <= 20){
+//     pontos = Number(hora) * 5;
+// }else{
+//     pontos = Number(hora) * 10;
+// }
+
+// resultado = pontos * ganhou;
+
+// alert("Você ganhou: " + "R$ " + resultado
+// )
+
+
+// _______________________________________________________
+
+
+let sal = prompt("Qual seu salario atual: ");
+let sexo = prompt("Qual seu gênero? M  para Masculino e F para Feminino ")
+let anos = prompt("Quantos anos você faz parte da empresa? ")
+let reajuste, salf;
+
+sexo = sexo.toUpperCase();
+
+if(sexo == "F"){
+    if(anos <= 15){
+        reajuste = (Number(sal) * 0.05);
+    }else if(anos > 15 && anos <= 20){
+        reajuste = (Number(sal) * 0.120);
+    }else{
+        reajuste = (Number(sal) * 0.23);
+    }
+}else if(sexo == "M"){
+    if(anos <= 15){
+        reajuste = (Number(sal) * 0.03);
+    }else if(anos > 15 && anos <= 20){
+        reajuste = (Number(sal) * 0.123);
+    }else{
+        reajuste = (Number(sal) * 0.25);
+    }
 }
+
+salf = Number(sal) + reajuste;
+
+alert("Seu Reajuste foi de: " + "R$" + reajuste + "\nSalario atual com o reajuste ficou: " + "R$" + salf);
